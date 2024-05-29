059c7ef8aa62f76ae90db59e165280bb7e8dab8f
jairowong <jairozz200@gmail.com>
2024-05-28 21:06:29 -0500

    constructor

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,15 @@
+import com.iroman.entity.Category;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-      
+        Category category= new Category();
+        category.setId(1L);
+        category.setDescription("descripcion");
+        category.setName("name");
 
+        System.out.println("categoria es:"+ category.getName());
     }
 }
